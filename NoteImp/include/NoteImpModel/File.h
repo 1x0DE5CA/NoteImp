fe3920ce65985a9d3c79cca23112bf7427260728
Artem Leshchenko <art.leshch@gmail.com>
2024-09-25 13:04:49 +0300

    move all related to files code to its separate class

--- NoteImp/include/NoteImpModel/File.h
+++ NoteImp/include/NoteImpModel/File.h
@@ -1,20 +1,29 @@
 #ifndef FILE_H
 #define FILE_H
 
-#include <unordered_map>
-#include <ctime>
+#include <stdexcept>
+
+#include <QString>
+#include <QFile>
+#include <QTextStream>
+#include <QFileInfo>
 
 namespace NoteImp {
     namespace Model {
         class File
         {
         public:
-            File();
-            ~File();
+            File(const QString &fileName);
+            const QString getFileName();
+            const QString getContent();
 
         private:
-            //std::unordered_map<std::ctime, QString>* fileHistory;
+            const QString fileName;
+            QString content;
+            QFileInfo fileInfo;
+            bool isModified;
 
+            void readFromFile();
         };
     }
 }
--- NoteImp/src/NoteImpModel/File.cpp
+++ NoteImp/src/NoteImpModel/File.cpp
@@ -2,8 +2,29 @@
 
 using namespace NoteImp::Model;
 
-File::File()
-{}
+File::File(const QString &fileName)
+    : fileName(fileName), fileInfo(fileName), isModified(false)
+{
+    readFromFile();
+}
 
-File::~File()
-{}
+void File::readFromFile()
+{
+    QFile file(fileName);
+    if (!file.open(QIODevice::ReadOnly | QFile::Text)) {
+        throw std::runtime_error(file.errorString().toStdString());
+    }
+    QTextStream in(&file);
+    content = in.readAll();
+    file.close();
+}
+
+const QString File::getFileName()
+{
+    return fileInfo.fileName();
+}
+
+const QString File::getContent()
+{
+    return content;
+}
